d4d2e41fd42c6601734b2200ad81913c1880fec7
Mitro <alessio.mitrotta@gmail.com>
2018-07-03 11:36:34 +0200

    initial setup

---
+++ src/main/java/com/mitro/EmployeeConsumerApplication.java
@@ -0,0 +1,35 @@
+package com.mitro;
+
+import com.mitro.controller.ConsumerControllerClient;
+import com.mitro.stream.GreetingsStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClientException;
+
+import java.io.IOException;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+public class EmployeeConsumerApplication {
+
+	public static void main(String[] args) throws RestClientException, IOException {
+		ApplicationContext ctx = SpringApplication.run(EmployeeConsumerApplication.class, args);
+
+		ConsumerControllerClient consumerControllerClient=ctx.getBean(ConsumerControllerClient.class);
+		System.out.println(consumerControllerClient);
+		consumerControllerClient.getEmployee();
+		consumerControllerClient.getCiao();
+	}
+
+	@Bean
+	public  ConsumerControllerClient  consumerControllerClient()
+	{
+		return  new ConsumerControllerClient();
+	}
+}
---
+++ src/main/java/com/mitro/service/GreetingsService.java
@@ -0,0 +1,39 @@
+package com.mitro.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.SubscribableChannel;
+import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
+import com.mitro.model.Greetings;
+import com.mitro.stream.GreetingsStream;
+
+/**
+ * Created by AlessioMitrotta on 26-Jun-18.
+ * If you have any problem with this code please let me know at amitrotta@soldo.com
+ */
+@Service
+public class GreetingsService {
+
+    Logger logger = LoggerFactory.getLogger(GreetingsService.class);
+
+    @Autowired
+    GreetingsStream greetingsStream;
+
+    public void sendGreeting(final Greetings greetings) {
+        MessageChannel messageChannel = greetingsStream.outboundGreetings();
+        messageChannel.send(MessageBuilder
+                .withPayload(greetings)
+                .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+                .build());
+    }
+
+    public void receiveGreetings(final Greetings greetings) {
+        SubscribableChannel subscribableChannel = greetingsStream.inboundGreetings();
+        subscribableChannel.subscribe(message -> logger.info("receiveGreetings {}", message));
+    }
+}
